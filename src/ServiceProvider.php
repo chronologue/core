244a433e266d4d5fda0e1c4087508eb2c06fc358
Darma <fibodarma@gmail.com>
2025-07-08 09:32:05 +0800

    update service provider

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -17,21 +17,43 @@ class ServiceProvider extends ModuleServiceProvider
 {
     public function register(): void
     {
+        parent::register();
+        
+        $this->registerControllerDispatcher();
+        $this->registerLengthAwarePaginator();
+        $this->registerBlueprint();
+        $this->resolvingController();
+        $this->resolvingService();
+    }
+
+    protected function registerControllerDispatcher():void
+    {
         $this->app->singleton(ControllerDispatcher::class, function ($app) {
             return new ControllerInvoker($app);
         });
+    }
 
+    protected function registerLengthAwarePaginator():void
+    {
         $this->app->bind(LengthAwarePaginator::class, Paginator::class);
-        $this->app->bind(Blueprint::class, Prototype::class);
+    }
 
+    protected function registerBlueprint():void
+    {
+        $this->app->bind(Blueprint::class, Prototype::class);
+    }
+
+    protected function resolvingController():void
+    {
         $this->app->resolving(Controller::class, function (Controller $controller, Application $app) {
             $controller->boot($app);
         });
+    }
 
+    protected function resolvingService():void
+    {
         $this->app->resolving(Service::class, function (Service $service, Application $app) {
             $service->boot($app);
         });
-
-        parent::register();
     }
 }
