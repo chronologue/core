48ff6ead5ffcf6e16a769c4c6f869c496a7aae4e
Darma <fibodarma@gmail.com>
2025-07-15 08:57:57 +0800

    add ResolvesAttributeCasts trait

---
+++ src/Database/Eloquent/Casts/ResolvesAttributeCasts.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Chronologue\Core\Database\Eloquent\Casts;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @mixin Model
+ */
+trait ResolvesAttributeCasts
+{
+    protected function resolveCustomAttribute(string $key, bool $cast = true)
+    {
+        return value(new Attribute(
+            fn($value, $attributes) => $this->resolveAttributeCasting($attributes, $key, $cast),
+            fn($value) => [$key => $value]
+        ));
+    }
+
+    protected function resolveAttributeCasting(array $attributes, string $key, bool $cast)
+    {
+        $value = $attributes[$key] ?? null;
+        return $cast ? $this->castAttribute($key, $value) : $value;
+    }
+}
